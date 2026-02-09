cffee7c232498e09725e59033400ccda74aad557
pradyumn tendulkar <55057911+pradyten@users.noreply.github.com>
2026-02-09 03:20:51 -0500

    Time: 5 ms (41.96%) | Memory: 19.3 MB (56.82%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,30 +1,20 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
-        n = 9
-        for i in range(9):
-            count = defaultdict(int)
-            for j in range(9):
-                if board[i][j] in count and board[i][j] != '.':
-                    return False
-                count[board[i][j]] = 1
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)
 
-        for i in range(9):
-            count = defaultdict(int)
-            for j in range(9):
-                if board[j][i] in count and board[j][i] != '.':
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if ( board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]):
                     return False
-                count[board[j][i]] = 1
 
-        for square in range(9):
-            count = defaultdict(int)
-            for i in range(3):
-                for j in range(3):
-                    row = (square // 3) * 3 + i
-                    col = (square % 3) * 3 + j
-                    if board[row][col] in count and board[row][col] != '.':
-                        return False
-                    count[board[row][col]] = 1
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
 
         return True
-        
