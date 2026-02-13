6d8aff6e7dbf821fd99c40539974694287d81eb1
pradyumn tendulkar <55057911+pradyten@users.noreply.github.com>
2026-02-12 23:19:07 -0500

    Time: 440 ms (93.61%) | Memory: 22.3 MB (33.83%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,24 +1,28 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        count = defaultdict(int)
-        for num in nums:
-            count[num] += 1
-
         res = []
-        for i in range(len(nums)):
-            count[nums[i]] -= 1
-            if i and nums[i] == nums[i - 1]:
+        nums.sort()
+        print(nums)
+
+        for i, a in enumerate(nums):
+            if a > 0:
+                break
+
+            if i > 0 and a == nums[i - 1]:
                 continue
 
-            for j in range(i + 1, len(nums)):
-                count[nums[j]] -= 1
-                if j - 1 > i and nums[j] == nums[j - 1]:
-                    continue
-                target = -(nums[i] + nums[j])
-                if count[target] > 0:
-                    res.append([nums[i], nums[j], target])
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
 
-            for j in range(i + 1, len(nums)):
-                count[nums[j]] += 1
         return res
